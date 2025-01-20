0cec240a5156b6fed024b0ca19110f995c6fa431
Malevych Yurii <yuriimalevych@gmail.com>
2025-01-20 21:28:17 +0200

    add hover to buttons, links + add up_arrow script

---
+++ src/js/upArrow.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopButton = document.getElementById("scrollToTop");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollToTopButton.classList.add("show");
+        } else {
+            scrollToTopButton.classList.remove("show");
+        }
+    });
+
+    scrollToTopButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
